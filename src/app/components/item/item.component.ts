f9f43dfbc52803c900bbae7325e22aef19f7d265
José Cardoso <120715230+TypeSS@users.noreply.github.com>
2023-05-10 21:39:16 +0100

    Correções
    Melhorei aspetos da homepage e comecei a trabalhar nos itens do menu

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
 
+export class ItemComponent {
+@Input() itemImg: string = "";
+@Input() Title:string = "";
+@Input() Descricao:string = "";
+@Input() Preco: string = "";
 }
